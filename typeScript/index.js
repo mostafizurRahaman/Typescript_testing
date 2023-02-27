1a00a885ddb467dc4045504ef6e427baaebe3751
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-02-28 00:30:39 +0600

    note : maintain a better folder sturcture like src for ts files  and dist folder for js files

--- typeScript/index.js
+++
@@ -1,5 +0,0 @@
-var Name = "Cumilla Polytechnic Institute";
-console.log(Name);
-var Language = "Typescript";
-var reason = "typescript is an super script of javascript. It's helps us to main types in javascript.";
-console.log(reason);

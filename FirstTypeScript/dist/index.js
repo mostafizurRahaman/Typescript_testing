2e41ad3774ba58708f81c84f826df0a31c767e36
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-02-28 00:38:25 +0600

    feat use: tsc command or tsc --w (typescirpt watch every mode )

---
+++ FirstTypeScript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const Name = "Cumilla Polytechnic Institute";
+console.log(Name);
+const Language = "Typescript";
+const reason = "typescript is an super script of javascript. It's helps us to main types in javascript.";
+console.log(reason);
+const n = 30;
+console.log(n);
--- FirstTypeScript/src/index.ts
+++ FirstTypeScript/src/index.ts
@@ -5,3 +5,6 @@ const Language = "Typescript";
 const reason = "typescript is an super script of javascript. It's helps us to main types in javascript."; 
 
 console.log(reason); 
+
+const n  = 30; 
+console.log(n); 

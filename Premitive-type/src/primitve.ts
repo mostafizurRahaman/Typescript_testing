a0eded60c6f6afeb7335d5985fe33557f1cd4e21
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-02-28 12:19:05 +0600

    feat: learn union type in typescript

---
+++ Premitive-type/src/primitve.ts
@@ -0,0 +1,38 @@
+//  we Discuss here primitive  types && Union Type in typescript :
+let school: string;
+school = "Bamni Adarsha High school";
+// the variable type is string if we want store another types data in the variable, typescript gives an errors : example :
+// school = 5; // gives error because type are not matched .
+
+let age: number; // only allows  number not string , boolean or null or undefined or any types
+age = 2;
+// age = false; // gives error because the type are not matched.
+
+let isGf: boolean;
+isGf = false;
+isGf = true;
+// isGf = 3; //gives error the because the type is not matched.
+
+/* ==================================
+      Union Types 
+================================== */
+
+// Union Types: Union allows to define  multiple specific types for variable .
+// for define multiple types we can use or sign (|);
+
+let myFatherAge: string | number | boolean; // the variable allows three types of data now because we define the variable types are string , number , boolean ;
+
+myFatherAge = "Fifty one";
+myFatherAge = 51;
+myFatherAge = true;
+
+
+let isSister: string | number | boolean; 
+isSister  = 'two'; 
+isSister = 2; 
+isSister = true; 
+isSister = NaN; // allows because the type of NaN is number 
+isSister = [] // gives errors because the type is array . array is not allows here . 
+
+
+
